a06f210958e6b26d7c34b26ba5393bd9ab51c06d
hajoon22 <hajoon@hajoon.me>
2026-06-16 11:37:56 +0900

    Create icmp.h

---
+++ src/icmp/icmp.h
@@ -0,0 +1,18 @@
+#ifndef ICMP_H
+#define ICMP_H
+
+#define MAX_DATA_BUFFER 1500
+
+struct icmp_unreach {
+    struct iphdr iph;
+    struct icmphdr icmph;
+
+    uint8_t *data;
+    size_t data_len;
+};
+
+void deinit_icmp_unreach(struct icmp_unreach *rp);
+struct icmp_unreach *read_icmp_unreach(int s);
+int send_icmp_unreach(int s, uint32_t saddr, uint16_t sport, uint32_t daddr, uint16_t dport, uint8_t *data, size_t len);
+
+#endif
