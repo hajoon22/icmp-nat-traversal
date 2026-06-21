5dd306d6e28652ed8b58691a741418083bb74545
hajoon22 <hajoon@hajoon.me>
2026-06-21 11:08:13 +0900

    Refactor main.c to use traversal session for STUN

--- src/server/main.c
+++ src/server/main.c
@@ -1,47 +1,31 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdint.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <poll.h>
-#include <netinet/ip_icmp.h>
 
-#include "../stun/stun.h"
 #include "../icmp/icmp.h"
+#include "../traversal/traversal.h"
 
 int main() {
-    uint32_t addr = 0;
-    uint16_t port = 0;
+    uint32_t stun_address = ntohl(inet_addr("74.125.250.129"));
+    uint16_t stun_port = 19302;
 
-    int udp_s = get_stun(ntohl(inet_addr("74.125.250.129")), 19302, &addr, &port);
-    if (udp_s < 0) return udp_s;
-    
-    struct in_addr a;
-    a.s_addr = htonl(addr);
-    printf("udp mapped: public addr=%s, public sport=%d\n", inet_ntoa(a), port);
-
-    int icmp_s = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if (icmp_s < 0) return icmp_s;
-
-	struct pollfd pfd = {
-		.fd = icmp_s,
-		.events = POLLIN
-	};
-
-    while (1) {
-        send(udp_s, "aaaaa", 5, 0); // keepalive
-        
-        int n = poll(&pfd, 1, 1000);
-		if (n < 0) break;
-        if (n > 0) {
-            struct icmp_unreach *rp = read_icmp_unreach(icmp_s);
-            if (!rp) continue;
-            printf("%.*s\n", (int)rp->data_len, rp->data);
-            deinit_icmp_unreach(rp);
-        }
+    struct traversal_session ts;
+    if (new_traversal_session(&ts, stun_address, stun_port) < 0) {
+        return -1;
     }
     
-    close(udp_s);
-    close(icmp_s);
+    struct in_addr a;
+    a.s_addr = htonl(ts.public_address);
+    printf("udp mapped: public addr=%s, public sport=%d\n", inet_ntoa(a), ts.mapped_port);
+
+    while (1) {
+        struct icmp_unreach *rp = traversal_read(&ts, 1000);
+        if (!rp) continue;
+
+        printf("%.*s\n", (int)rp->data_len, rp->data);
+        deinit_icmp_unreach(rp);
+    }
+    deinit_traversal_session(&ts);
 
     return 0;
 }
