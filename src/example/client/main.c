e8838592c3c1e80264bab4c30a783a4cc20ebf60
hajoon22 <hajoon@hajoon.me>
2026-07-01 09:09:30 +0900

    Implement main function for traversal client

---
+++ src/example/client/main.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <arpa/inet.h>
+
+#include "../../traversal/icmp/icmp.h"
+#include "../../traversal/traversal.h"
+
+int main() {
+    uint32_t stun_address = ntohl(inet_addr("74.125.250.129"));
+    uint16_t stun_port = 19302;
+
+    struct traversal_session ts;
+    if (new_traversal_session(&ts, stun_address, stun_port) < 0) {
+        return -1;
+    }
+
+    uint32_t dst = ntohl(inet_addr("1.1.1.1")); // target's public ip
+    uint16_t dst_port = 54995; // target's mapped public port
+
+    traversal_send(&ts, dst, dst_port, "helloicmp", 9);
+    deinit_traversal_session(&ts);
+}
