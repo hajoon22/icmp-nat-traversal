125fbba75833f7db026e1a40197980f9d0e4c8fc
hajoon22 <hajoon@hajoon.me>
2026-07-10 16:51:31 +0900

    Remove stun_addr and stun_port from new_traversal_session

--- src/traversal/traversal.h
+++ src/traversal/traversal.h
@@ -22,7 +22,7 @@ struct traversal_session {
 };
 
 void deinit_traversal_session(struct traversal_session *ts);
-int new_traversal_session(struct traversal_session *ts, uint32_t stun_addr, uint16_t stun_port);
+int new_traversal_session(struct traversal_session *ts);
 int traversal_read(struct traversal_session *ts, struct icmp_unreach *icmpun, int timeout);
 int traversal_send(struct traversal_session *ts, uint32_t dst, uint16_t dst_port, uint8_t *data, size_t len);
 
